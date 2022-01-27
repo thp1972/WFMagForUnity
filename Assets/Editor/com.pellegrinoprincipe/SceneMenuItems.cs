68c725c143cd7c2ff663b7448534b2d0c41b55ab
thp1972 <gnu_tux@yahoo.it>
2022-01-27 19:13:33 +0100

    NR_24 WIP

--- Assets/Editor/com.pellegrinoprincipe/SceneMenuItems.cs
+++ Assets/Editor/com.pellegrinoprincipe/SceneMenuItems.cs
@@ -7,157 +7,157 @@ namespace com.pellegrinoprincipe
     public class SceneMenuItems
     {
 		[MenuItem("Assets/Select Scene/New Scene", false, 0)]
-		static void Scene1053349742()
+		static void Scene1935246878()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/__TEST__/New Scene.unity");
 		}
 		[MenuItem("Assets/Select Scene/Main", false, 0)]
-		static void Scene1449092281()
+		static void Scene466499017()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/MAIN/Scenes/Main.unity");
 		}
 		[MenuItem("Assets/Select Scene/Explosion", false, 0)]
-		static void Scene837558164()
+		static void Scene2088586980()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_001/Scenes/Explosion.unity");
 		}
 		[MenuItem("Assets/Select Scene/Galaxian", false, 0)]
-		static void Scene1851722589()
+		static void Scene600726061()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_002/Scenes/Galaxian.unity");
 		}
 		[MenuItem("Assets/Select Scene/ParallaxScrolling", false, 0)]
-		static void Scene1893238678()
+		static void Scene1028249318()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_003/Scenes/ParallaxScrolling.unity");
 		}
 		[MenuItem("Assets/Select Scene/ThrusterMotion", false, 0)]
-		static void Scene1680739141()
+		static void Scene700406325()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_004/Scenes/ThrusterMotion.unity");
 		}
 		[MenuItem("Assets/Select Scene/HighScore", false, 0)]
-		static void Scene2081243816()
+		static void Scene832312280()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_005/Scenes/HighScore.unity");
 		}
 		[MenuItem("Assets/Select Scene/Tail", false, 0)]
-		static void Scene2081680313()
+		static void Scene830651081()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_006/Scenes/Tail.unity");
 		}
 		[MenuItem("Assets/Select Scene/JumpPhysics", false, 0)]
-		static void Scene2028979942()
+		static void Scene897488790()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_007/Scenes/JumpPhysics.unity");
 		}
 		[MenuItem("Assets/Select Scene/Missiles", false, 0)]
-		static void Scene236024987()
+		static void Scene1132471787()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_008/Scenes/Missiles.unity");
 		}
 		[MenuItem("Assets/Select Scene/Invaders", false, 0)]
-		static void Scene453867534()
+		static void Scene1453074814()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_009/Scenes/Invaders.unity");
 		}
 		[MenuItem("Assets/Select Scene/Spawn", false, 0)]
-		static void Scene550300750()
+		static void Scene1834859838()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_010/Scenes/Spawn.unity");
 		}
 		[MenuItem("Assets/Select Scene/Breakout", false, 0)]
-		static void Scene1414680383()
+		static void Scene432217679()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_011/Scenes/Breakout.unity");
 		}
 		[MenuItem("Assets/Select Scene/Bombs", false, 0)]
-		static void Scene1440144308()
+		static void Scene407341764()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_012/Scenes/Bombs.unity");
 		}
 		[MenuItem("Assets/Select Scene/Stars", false, 0)]
-		static void Scene1344164909()
+		static void Scene495912285()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_013/Scenes/Stars.unity");
 		}
 		[MenuItem("Assets/Select Scene/WalkCycle", false, 0)]
-		static void Scene1483582406()
+		static void Scene368900790()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_014/Scenes/WalkCycle.unity");
 		}
 		[MenuItem("Assets/Select Scene/IsometricMap", false, 0)]
-		static void Scene860099371()
+		static void Scene2127905371()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_015/Scenes/IsometricMap.unity");
 		}
 		[MenuItem("Assets/Select Scene/SpriteFollowing", false, 0)]
-		static void Scene2123344936()
+		static void Scene857668952()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_016/Scenes/SpriteFollowing.unity");
 		}
 		[MenuItem("Assets/Select Scene/Lemmings", false, 0)]
-		static void Scene1655238395()
+		static void Scene792313739()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_017/Scenes/Lemmings.unity");
 		}
 		[MenuItem("Assets/Select Scene/MonsterMaze", false, 0)]
-		static void Scene1806260858()
+		static void Scene641207050()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_018/Scenes/MonsterMaze.unity");
 		}
 		[MenuItem("Assets/Select Scene/Continue", false, 0)]
-		static void Scene1830668489()
+		static void Scene546085305()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_019/Scenes/Continue.unity");
 		}
 		[MenuItem("Assets/Select Scene/Shooting", false, 0)]
-		static void Scene614302079()
+		static void Scene1762447375()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_020/Scenes/Shooting.unity");
 		}
 		[MenuItem("Assets/Select Scene/Supersprint", false, 0)]
-		static void Scene242986282()
+		static void Scene1139473498()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_021/Scenes/Supersprint.unity");
 		}
 		[MenuItem("Assets/Select Scene/Scramble", false, 0)]
-		static void Scene433884861()
+		static void Scene1414389709()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_022/Scenes/Scramble.unity");
 		}
 		[MenuItem("Assets/Select Scene/Sprint", false, 0)]
-		static void Scene1636675334()
+		static void Scene740097654()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_023/Scenes/Sprint.unity");
 		}
 		[MenuItem("Assets/Select Scene/DonkeyKong", false, 0)]
-		static void Scene689067259()
+		static void Scene1686308235()
 		{
 			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 			EditorSceneManager.OpenScene("Assets/NR_024/Scenes/DonkeyKong.unity");
--- Assets/NR_024/Scripts/DonkeyKong.cs
+++ Assets/NR_024/Scripts/DonkeyKong.cs
@@ -2,16 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using PygameZero;
+using System.Linq;
 
 namespace NR_024
 {
+    class Barrel : Actor
+    {
+        public float frame;
+
+        public Barrel(string image, (float, float) pos, SpriteUtility.PivotPosition anchor) :
+            base(image, pos, anchor)
+        {
+            SortingOrder = 2;
+        }
+    }
+
+
     public class DonkeyKong : MonoBehaviour
     {
         public GameObject backgroundPrefab;
         GameObject background;
 
-
-        List<Actor> barrels = new List<Actor>();
+        List<Barrel> barrels = new List<Barrel>();
         Image platformMap;
         int spacer = 0;
 
@@ -27,12 +39,51 @@ namespace NR_024
         private void Draw()
         {
             ScreenUtility.Blit(background, (0, 0));
+
+            foreach (var b in Enumerable.Range(0, barrels.Count))
+            {
+                if (OnScreen((int)barrels[b].X, (int)barrels[b].Y))
+                {
+                    barrels[b].Draw();
+                }
+            }
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (MathUtility.RandInt(0, 100) == 1 && spacer < 0)
+            {
+                MakeBarrel();
+                spacer = 100;
+            }
+
+            spacer -= 1;
+
+
             Draw();
+        }
+
+        bool OnScreen(int x, int y)
+        {
+            return Enumerable.Range(16, 784 - 16).Contains(x) && Enumerable.Range(16, 584 - 16).Contains(y);
+        }
+
+        void MakeBarrel()
+        {
+            barrels.Add(new Barrel("bfrfront1", (200, 30), SpriteUtility.PivotPosition.CENTER));
+            barrels[barrels.Count - 1].frame = 1;
+        }
+
+        int TestPlatform(int x, int y, int col)
+        {
+            int c = 0;
+            foreach (var z in Enumerable.Range(0, 3))
+            {
+                var rgb = platformMap.GetAt(x, y + z);
+                c += rgb[col];
+            }
+            return c;
         }
     }
 }
